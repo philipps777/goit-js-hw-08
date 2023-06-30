254da4ad97dbf36e36f6166c2e2c5cc11d686300
philips777 <mymailforall@ukr.net>
2023-06-30 17:56:27 +0300

    first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,50 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+// // Change code below this line
 
-console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery')
+
+
+const markup = galleryItems.map(({original, preview, description}) =>`<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>
+</li>`).join(" ")
+
+gallery.insertAdjacentHTML("beforeend", markup)
+
+gallery.addEventListener('click', onClick)
+
+let instance = null
+
+function onClick(evt) {  
+  
+  evt.preventDefault();
+
+  if (evt.target.classList.contains('gallery__image')) {
+    
+    const source = evt.target.dataset.source;
+   
+    const modalImage = `<img src="${source}">`;
+
+    instance = basicLightbox.create(modalImage);
+   
+    instance.show();
+    
+  }
+
+}
+window.addEventListener('keydown', evt => {
+  if (evt.key === 'Escape') {
+    instance.close();
+  }
+});
+
+
