e3b7a44d93f8fac35b5aac28dc1cd8e988bc4380
philips777 <mymailforall@ukr.net>
2023-06-30 23:16:47 +0300

    fist task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,50 +1,25 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 import SimpleLightbox from "simplelightbox";
-// // Change code below this line
-
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
+// Change code below this line
 
 const gallery = document.querySelector('.gallery')
 
 
-const markup = galleryItems.map(({original, preview, description}) =>`<li class="gallery__item">
+const markup = galleryItems.map(({original, preview, description}) =>` <li class="gallery__item">
 <a class="gallery__link" href="${original}">
-  <img
-    class="gallery__image"
-    src="${preview}"
-    data-source="${original}"
-    alt="${description}"
-  />
+  <img class="gallery__image" src="${preview}" alt="${description}" />
 </a>
 </li>`).join(" ")
 
 gallery.insertAdjacentHTML("beforeend", markup)
 
-gallery.addEventListener('click', onClick)
 
-let instance = null
 
-function onClick(evt) {  
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+      });  
   
-  evt.preventDefault();
-
-  if (evt.target.classList.contains('gallery__image')) {
-    
-    const source = evt.target.dataset.source;
-   
-    const modalImage = `<img src="${source}">`;
-
-    instance = basicLightbox.create(modalImage);
-   
-    instance.show();
-    
-  }
-
-}
-window.addEventListener('keydown', evt => {
-  if (evt.key === 'Escape') {
-    instance.close();
-  }
-});
-
-
