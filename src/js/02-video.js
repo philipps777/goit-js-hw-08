e9529cfaeb80c407cbe5247f5b2fa943e304162d
philips777 <mymailforall@ukr.net>
2023-07-04 13:52:09 +0300

    Modul 8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,47 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const LS = 'videoplayer-current-time'
+
+const saveCurrentTimeToLocalStorage = function (time) {
+  const data = JSON.stringify(time);
+  localStorage.setItem(LS, data); 
+  
+}
+
+const loadCurrentTimeFromLocalStorage = function () {
+    const data = localStorage.getItem(LS);    
+    return JSON.parse(data);
+ 
+};
+
+const updateCurrentTimeThrottled = throttle(saveCurrentTimeToLocalStorage, 1000);
+
+
+const onPlay = function(event) {
+  const currentTime = event.seconds;
+  saveCurrentTimeToLocalStorage(currentTime);
+};
+
+player.on('timeupdate', onPlay);
+
+const savedTime = JSON.parse(localStorage.getItem(LS));
+
+if (savedTime) {
+  player.setCurrentTime(savedTime).catch(function(error) {
+    switch (error.name) {
+      case 'RangeError':
+        
+        break;
+      default:
+        break;
+    }
+  });
+}
+
+
+player.play();
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,51 @@
 
+import throttle from 'lodash.throttle';
+
+
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+
+
+const LS = 'feedback-form-state'
+
+const saveFormState = () => {
+  const formData = {
+    email: emailInput.value,
+    message: messageInput.value
+  };
+  localStorage.setItem(LS, JSON.stringify(formData));
+};
+
+
+const loadFormState = () => {
+  const savedData = localStorage.getItem(LS);
+  if (savedData) {
+    const formData = JSON.parse(savedData);
+    emailInput.value = formData.email;
+    messageInput.value = formData.message;
+  }
+};
+
+
+const resetFormState = () => {
+  localStorage.removeItem(LS);
+  emailInput.value = '';
+  messageInput.value = '';
+};
+
+
+const handleFormInput = throttle(saveFormState, 500);
+
+
+form.addEventListener('input', handleFormInput);
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  console.log({
+    email: emailInput.value,
+    message: messageInput.value
+  });
+  resetFormState();
+});
+
+loadFormState();
