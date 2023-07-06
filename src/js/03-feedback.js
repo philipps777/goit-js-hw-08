49e1fe2322b46965bafc8b493cee56a8737274b0
philips777 <mymailforall@ukr.net>
2023-07-06 08:20:28 +0300

    fix 03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,36 +2,40 @@
 import throttle from 'lodash.throttle';
 
 
-const form = document.querySelector('.feedback-form');
-const emailInput = form.querySelector('input[name="email"]');
-const messageInput = form.querySelector('textarea[name="message"]');
+const formEl = document.querySelector('.feedback-form');
+// const emailInput = form.querySelector('input[name="email"]');
+// const messageInput = form.querySelector('textarea[name="message"]');
 
 
-const LS = 'feedback-form-state'
+const LS = 'feedback-form-state';
 
-const saveFormState = () => {
-  const formData = {
-    email: emailInput.value,
-    message: messageInput.value
-  };
+let formData = {};
+
+const saveFormState = (e) => {
+  formData[e.target.name] = e.target.value.trim();
+  // const formData = {
+  //   email: emailInput.value,
+  //   message: messageInput.value
+  // };
   localStorage.setItem(LS, JSON.stringify(formData));
 };
 
 
-const loadFormState = () => {
-  const savedData = localStorage.getItem(LS);
-  if (savedData) {
-    const formData = JSON.parse(savedData);
-    emailInput.value = formData.email;
-    messageInput.value = formData.message;
-  }
-};
+// const loadFormState = () => {
+//   const savedData = localStorage.getItem(LS);
+//   if (savedData) {
+//     const formData = JSON.parse(savedData);
+//     emailInput.value = formData.email;
+//     messageInput.value = formData.message;
+//   }
+// };
 
 
 const resetFormState = () => {
   localStorage.removeItem(LS);
-  emailInput.value = '';
-  messageInput.value = '';
+  formEl.reset();
+  // emailInput.value = '';
+  // messageInput.value = '';
 };
 
 
@@ -41,11 +45,28 @@ const handleFormInput = throttle(saveFormState, 500);
 form.addEventListener('input', handleFormInput);
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  console.log({
-    email: emailInput.value,
-    message: messageInput.value
-  });
+  console.log(formData);
+  formData = {};
+  // console.log({
+  //   email: emailInput.value,
+  //   message: messageInput.value
+  // });
   resetFormState();
 });
 
-loadFormState();
+// loadFormState();
+
+const onLoad = () => {
+  try {
+    const data = localStorage.getItem(LS);
+    if(!data) return;
+    formData = JSON.parse(data);
+    Object.entries(formData).forEach(([key, val]) =>{
+      formEl.elements[key].value = val
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+window.addEventListener("load", onLoad)
